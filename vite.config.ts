eff1ec0cb72b66437f5295e390bb86a18e2e0aa1
JusteKal <contact.justekal@gmail.com>
2025-06-25 19:24:36 +0200

    update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Justemoi/', // <-- Remplace <nom-du-repo> par le nom de ton dépôt GitHub
   plugins: [
     vue(),
     vueDevTools(),
